055a9f72decf464e8a6a10f458d4e2dc6b011e0f
SamuelW05 <123250590+SamuelW05@users.noreply.github.com>
2023-05-23 18:12:07 +0200

    Delete project_C3 directory

--- project_C3/id/id.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROJECT_C2_ID_H
-#define PROJECT_C2_ID_H
-
-unsigned int get_next_id();
-
-#endif //PROJECT_C2_ID_H
--- project_C3/menu/menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROJECT_C2_MENU_H
-#define PROJECT_C2_MENU_H
-#include "../shapes/shapes.h"
-#include "../area/area.h"
-
-void read_input(char* input);
-int compare_string(char* str, Area* draw_zone, LIST * l, int * current_ids);
-
-#endif //PROJECT_C2_MENU_H
